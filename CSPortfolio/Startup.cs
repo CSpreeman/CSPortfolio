9519e945943047b4ed3a1748656214a19a734400
Craig <craigspreeman@hotmail.com>
2017-06-20 11:50:16 -0700

    Add project files.

---
+++ CSPortfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSPortfolio.Startup))]
+namespace CSPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
